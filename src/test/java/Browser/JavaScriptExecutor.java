1ad7579d628c4d5e17b4b7a10dc1aedf5971b4e8
AlikDnepr <oleh.shlykov@gmail.com>
2025-08-09 20:38:47 +0300

    JavaScriptExecutor new version (Topic 8. JS Executor. String Functions, Functions in Java)

--- src/test/java/Browser/JavaScriptExecutor.java
+++ src/test/java/Browser/JavaScriptExecutor.java
@@ -6,31 +6,28 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
 public class JavaScriptExecutor {
     public static void main(String[] args) {
-        // WebDriver settings
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
 
         try {
-            // Open page
             driver.get("https://novaposhta.ua");
             driver.manage().window().maximize();
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 
             JavascriptExecutor js = (JavascriptExecutor) driver;
-            js.executeScript("window.scrollBy(0, 10000);"); // Scroll 10k pixels
 
-            Thread.sleep(2000);
+            scrollPageDown(js);
+            selectСountry(js, driver);
+            scrollPageUp(js);
+            fillSearchField(js, driver);
 
-            // The CSS selector is selected, which will be used to press the button
-            WebElement button = driver.findElement(By.cssSelector("#app-footer > div > div:nth-child(2) > div.flex.justify-between.items-center.l-xl\\:items-start.mt-10.gap-7.l-lg\\:flex-col-reverse.l-lg\\:items-start > div.flex.gap-8.items-center.l-md\\:flex-col.l-md\\:items-start > div > button")); // змінити селектор під потрібну кнопку
-            js.executeScript("arguments[0].click();", button);
-
-            Thread.sleep(2000);
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -38,4 +35,51 @@ public class JavaScriptExecutor {
             driver.quit();
         }
     }
+
+    public static void scrollPageDown(JavascriptExecutor js) throws InterruptedException {
+        js.executeScript("window.scrollBy(0, document.body.scrollHeight);");
+        Thread.sleep(1000);
+    }
+    public static void scrollPageUp(JavascriptExecutor js) throws InterruptedException {
+        js.executeScript("window.scrollBy(0, -document.body.scrollHeight);");
+        Thread.sleep(1000);
+    }
+
+    public static void fillSearchField(JavascriptExecutor js, WebDriver driver) throws InterruptedException {
+        WebElement search = driver.findElement(By.cssSelector("#main-page-search > div.np-input.w-full.h-full > label > div > input"));
+        js.executeScript("arguments[0].style.border='3px solid blue'", search);
+        js.executeScript(
+                "arguments[0].value='333-1527-1878';" +
+                        "arguments[0].dispatchEvent(new Event('input'));" +
+                        "arguments[0].dispatchEvent(new Event('change'));",
+                search
+        );
+        Thread.sleep(2000);
+        WebElement findButton = driver.findElement(By.cssSelector("#__nuxt > div > main > div > section > div >" +
+                " div.relative.z-1.h-full.app-container.flex.items-center > div > div.mt-10.flex.gap-3.l-lg-mid\\:flex-col > button"));
+        js.executeScript("arguments[0].style.border='3px solid green'", findButton);
+        Thread.sleep(2000);
+        findButton.click();
+        Thread.sleep(5000);
+    }
+
+    public static void selectСountry(JavascriptExecutor js, WebDriver driver) throws InterruptedException {
+        WebElement button = driver.findElement(By.cssSelector("#app-footer > div > div:nth-child(2) > " +
+                "div.flex.justify-between.items-center.l-xl\\:items-start.mt-10.gap-7.l-lg\\:flex-col-reverse.l-lg\\:items-start > div.flex.gap-8.items-center.l-md\\:flex-col.l-md\\:items-start > div > button"));
+        js.executeScript("arguments[0].style.border='3px solid blue'", button);
+        Thread.sleep(2000);
+        js.executeScript("arguments[0].click();", button);
+        Thread.sleep(2000);
+
+        WebElement close = driver.findElement(By.cssSelector("#app-footer > div > div:nth-child(2) > div.flex." +
+                "justify-between.items-center.l-xl\\:items-start.mt-10.gap-7.l-lg\\:flex-col-reverse.l-lg\\:items-start > div.flex.gap-8.items-center.l-md\\:flex-col.l-md\\:items-start > div > div > " +
+                "div > div.modal-content-wrapper.relative.z-10.max-h-\\[100\\%\\].h-full.max-w-\\[100vw\\].w-full." +
+                "flex.justify-center.pointer-events-none > div > div > div.absolute.right-\\[1\\.65rem\\].top-\\[1\\." +
+                "35rem\\].l-md\\:right-\\[0\\.8rem\\].l-md\\:top-\\[1\\.22rem\\] > button"));
+        js.executeScript("arguments[0].style.border='3px solid blue'", close);
+        Thread.sleep(2000);
+        js.executeScript("arguments[0].click();", close);
+        Thread.sleep(2000);
+    }
 }
+
--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -6,9 +6,46 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Test {
     public static void main(String[] args) {
+        ArrayList<String> emails = new ArrayList<>(List.of(
+                "Ivan.Petrov@@Gmail.Com",
+                "Katya_1989MAIL.ru",
+                "oleg#example.com",
+                "sergey@gnail.com",
+                "natalia@outlok.com",
+                "dmitriy@mail..ru",
+                "anna.smirnova@gmail.com",
+                "mikhail1988@yandex.ru",
+                "elena_kovalenko@outlook.com",
+                "roman-ivanov@mail.ru"
+        ));
+        ArrayList<String> refactoredEmails = new ArrayList<>();
+        for (int i = 0; i < emails.size(); i++) {
+            if (emails.get(i).contains(".ru")){
+                refactoredEmails.add(emails.get(i).replace(".ru",".ua"));
 
+            }
+            if (emails.get(i).equals(emails.get(i + 1))){
+                refactoredEmails.add(emails.get(i).replace(".ru",".ua"));
+            }
+
+            refactoredEmails.add(emails.get(i).toLowerCase());
+            //System.out.println(emails.get(i));
+        }
+        for (int i = 0; i < refactoredEmails.size(); i++) {
+            //System.out.println(refactoredEmails.get(i));
+        }
+
+        // Список
+        // Цикл
+        // Применение методов
+        // 1) Нижний регистр
+        // 2) Фильтр и разделение эмейлов по группам
+        // 3) Сравнение эмейлов между собой чтоб отклонить дубликаты
+        // 4) endsWith()
     }
 }
